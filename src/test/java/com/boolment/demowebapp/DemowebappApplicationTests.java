64f29f628a7ef3b425530b3c64e65c2064fc7673
Mukesh <mukesh@boolment.in>
2025-11-22 13:01:34 +0530

    First commit

---
+++ src/test/java/com/boolment/demowebapp/DemowebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boolment.demowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
